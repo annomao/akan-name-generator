f5f90d542900eb6b48eb6eece1a41d02fbe343d7
annomao <omaoannkerubo@gmail.com>
2022-03-06 02:06:29 +0300

    declare the various arrays

--- js/akan.js
+++ js/akan.js
@@ -3,7 +3,9 @@ function getAkanName(){
   let gender = document.getElementById("gender").value;
   let birthDate = document.getElementById("birth-date").value;
 
-
+  const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday", "Saturday"]
+  const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"]
+  const femaleNames = ["Akosua" ,"Adwoa","Abenaa",,"Akua","Yaa","Afua", "Ama"]
 
   /*let bDate = typeof birthDate
   alert(`${bDate}`)*/
