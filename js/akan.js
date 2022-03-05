92ff449115c9a80c2f4fefb8b20607939703325b
annomao <omaoannkerubo@gmail.com>
2022-03-06 00:03:21 +0300

    collect input values

---
+++ js/akan.js
@@ -0,0 +1,7 @@
+
+let fullName = document.getElementById("full-name").value;
+let gender = document.getElementById("gender").value;
+let birthDate = document.getElementById("birth-date").value;
+
+let submitButton = document.querySelector("#send");
+
