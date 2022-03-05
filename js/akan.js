a795f7a3a62a5a22ba8e2b60b4b3c18d0e65e2a2
annomao <omaoannkerubo@gmail.com>
2022-03-06 01:47:29 +0300

    get form inputs

--- js/akan.js
+++ js/akan.js
@@ -1,6 +1,15 @@
-/*let fullName = document.getElementById("full-name").value;
-let gender = document.getElementById("gender").value;
-let birthDate = document.getElementById("birth-date").value;*/
+function getAkanName(){
+  let fullName = document.getElementById("full-name").value;
+  let gender = document.getElementById("gender").value;
+  let birthDate = document.getElementById("birth-date").value;
+
+
+
+  /*let bDate = typeof birthDate
+  alert(`${bDate}`)*/
+}
+let formSubmit = document.getElementById("form-id")
+formSubmit.addEventListener('submit', getAkanName)
 
 
 
