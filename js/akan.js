5457695312400515ebe9790b03e9abc7859fd9e6
annomao <omaoannkerubo@gmail.com>
2022-03-07 01:10:48 +0300

    fix new date object bug

--- js/akan.js
+++ js/akan.js
@@ -4,11 +4,6 @@ let formSubmit = document.getElementById("form-id");
 formSubmit.addEventListener('submit', displayName);
 formSubmit.addEventListener('submit', handleForm);
 
-//prevents form from submitting hence prevents form refresh
-function handleForm(event) {
-  event.preventDefault(); 
- }
-
 function displayName(){
   message = getAkanName();
   results = document.getElementById("results");
@@ -27,23 +22,26 @@ function getAkanName(){
 //akan names stored in lists the names are stored in index corresponding to the days list
 
   const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"];
-  const femaleNames = ["Akosua" ,"Adwoa","Abenaa",,"Akua","Yaa","Afua", "Ama"];
+  const femaleNames = ["Akosua" ,"Adwoa","Abenaa","Akua","Yaa","Afua", "Ama"];
 
 /* splits date input use the arguments to create a javascript date object
 where the get day method returns a number that represents day of the week.
 refer to above comments */
   let birthDay = birthDate.split("-")
-  const birthday = new Date(birthDay[1],birthDay[2],birthDay[0]);
-  const day1 = birthday.getDay();
+  let birthday = new Date(parseInt(birthDay[0]),parseInt(birthDay[1])-1,parseInt(birthDay[2]));
+  let day1 = birthday.getDay();
 
   if (gender === "female") {
-    return `Hey ${fullName}, your Akan name is ${femaleNames[day1]}`;
+    return `Hey ${fullName}, your Akan name is ${femaleNames[day1]} and ${birthDate} ${birthday}`;
   }
   else{
-    return `Hey ${fullName}, your Akan name is ${maleNames[day1]}`;
+    return `Hey ${fullName}, your Akan name is ${maleNames[day1]} and ${birthday}`;
   }
 }
 
-/*     results = document.getElementById("results");
-    results.textContent = message;*/
+//prevents form from submitting hence prevents form refresh
+function handleForm(event) {
+  event.preventDefault(); 
+ }
+
 
